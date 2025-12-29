1e4ed1c5b0d942f24969811f90fa38de7d09f606
Yuganmetd <yugandhar.rachabathuni@gmail.com>
2025-12-29 16:53:12 +0530

    modified spec file name

---
+++ tests/Chapter01/RecordTest.spec.ts
@@ -0,0 +1,9 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('https://www.facebook.com/');
+  await page.getByTestId('royal-email').fill('testers talkt');
+  await page.getByTestId('royal-pass').fill('ersrer');
+  await page.getByTestId('royal-login-button').click();
+  await expect(page.locator('#email_container')).toContainText('The email address or mobile number you entered isn\'t connected to an account. Find your account and log in.');
+});
